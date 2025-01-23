b1b377ae2455b7298982fa76ea3d3b2fa7371613
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2025-01-23 18:57:50 -0300

    Alterado o height auto da select

--- app/components/selects/Select.jsx
+++ app/components/selects/Select.jsx
@@ -31,7 +31,7 @@ export function Select({ options, width, label, name, form, ...props }){
            style={{height: `${isOpen ? 20.3 * options.length : 0}px`, border: !isOpen && "none"}}
          >
             <ul className="flex flex-col"
-               style={{width: `${width}px`}}
+               style={{width: `${width}px`, height: height ? `${height}px` : "auto"}}
             >
                {
                   options.map(option => (
